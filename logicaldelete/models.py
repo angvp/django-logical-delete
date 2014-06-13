5b118dca3286fa0779cfe61060cc5080b89354c8
Cangiani Agustín <acangiani@routeatlas.com>
2014-06-13 12:37:20 -0300

    Fix date_removed < date_modified bug & Add undelete() method

--- logicaldelete/models.py
+++ logicaldelete/models.py
@@ -52,9 +52,41 @@ class Model(models.Model):
         self.date_removed = timezone.now()
         self.save()
 
+    def undelete(self):
+        '''
+        UnSoft delete all fk related objects that
+        inherit from logicaldelete class
+        '''
+
+        # Fetch related models
+        related_objs = [relation.get_accessor_name() for
+                        relation in self._meta.get_all_related_objects()]
+
+        for objs_model in related_objs:
+            # Retrieve all related objects
+            try:
+                obj = getattr(self, objs_model)
+            except ObjectDoesNotExist, AttributeError:
+                # The attribute  or relation  may not
+                # be instanciated.
+                pass
+
+            # Checking if inherits from logicaldelete
+            if not issubclass(obj.__class__, Model):
+                break
+            obj.undelete()
+
+        self.date_removed = None
+        self.save()
+
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         self.date_modified = timezone.now()
+
+        if self.date_removed:
+            # Patch for reupdating the DeletedTime
+            self.date_removed = timezone.now()
+
         super(Model, self).save(force_insert=False, force_update=False,
                                 using=None, update_fields=None)
 
