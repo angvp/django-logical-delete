cd3fc46ef8130616ba670d5c61d6593af7630b2c
Cangiani Agustín <acangiani@routeatlas.com>
2014-07-02 14:18:27 -0300

    Fix setup.py typo

--- setup.py
+++ setup.py
@@ -1,19 +1,20 @@
+# coding=utf-8
 from setuptools import find_packages, setup
 
 VERSION = __import__("logicaldelete").__version__
 
  
 setup(
-    name = 'django-logicaldelete',
-    version = VERSION,
-    author = u'Angel Velasquez, Agustín Cangiani',
-    author_email = 'angvp@archlinux.org, cangiani@gmail.com',
-    url = 'http://www.routeatlas.com',
-    description = u' '.join(__import__('logicaldelete').__doc__.splitlines()).strip(),
-    long_description = open("README.rst").read(),
-    packages = find_packages()
+    name='django-logicaldelete',
+    version=VERSION,
+    author=u'Angel Velasquez, Agustín Cangiani',
+    author_email='angvp@archlinux.org, cangiani@gmail.com',
+    url='http://www.routeatlas.com',
+    description=u' '.join(__import__('logicaldelete').__doc__.splitlines()).strip(),
+    long_description=open("README.rst").read(),
+    packages=find_packages(),
     license="BSD",
-    classifiers = [
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
